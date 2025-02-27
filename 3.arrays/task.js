04921e1f7539f07157657526155584483295dccf
vely-d <damyanovvk@gmail.com>
2025-02-27 17:57:11 +0300

    task 3 has been completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((element, index) => arr2[index] === element) && arr2.every((element, index) => arr1[index] === element);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let consideredUsers = users.filter(e => e.gender == gender);
+    if(consideredUsers.length === 0) {
+        return 0;
+    }
+    return consideredUsers.reduce((accumulator, currentValue) => accumulator + currentValue.age, 0) / consideredUsers.length;
 }
