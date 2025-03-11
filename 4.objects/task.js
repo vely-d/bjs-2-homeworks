c56df5427114853484641a015e18730bf05b2cdf
vely-d <damyanovvk@gmail.com>
2025-03-11 20:45:53 +0300

    objects homework is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (!this.hasOwnProperty("marks")) {
+    return;
+  }
+  // this.marks = [...(this.marks), ...marks];
+  this.marks = this.marks.concat(marks)
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.hasOwnProperty("marks") || this.marks.length === 0) {
+      return 0;
+    }
+    let sum = this.marks.reduce((a, b) => a + b);
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  this.excluded = reason;
 }
